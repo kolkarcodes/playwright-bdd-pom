6fa79990099144459f58c32f36f55251bbae46ab
kolkarcodes <kolkar.codes@gmail.com>
2026-04-10 01:15:50 +0100

    first BDD login scenario

---
+++ features/login.feature
@@ -0,0 +1,6 @@
+Feature: User Login
+Scenario: Successful login with valid credentials
+  Given the user is on the login page
+  When the user enters valid credentials
+  And clicks the login button
+  Then the user should be redirected to the my account page
---
+++ steps/loginSteps.ts
@@ -0,0 +1,47 @@
+import {Given, When,Then,Before,After} from "@cucumber/cucumber"; 
+import {chromium,Page, Browser, expect} from "@playwright/test";
+
+let page: Page;
+let browser: Browser;
+
+Before(async () => {
+    browser = await chromium.launch({headless: false});
+    const context = await browser.newContext();
+    page = await browser.newPage();
+} );
+
+After(async () => {
+    await browser.close();
+} );
+
+ Given('the user is on the login page', async () => {
+    await page.goto("https://practicesoftwaretesting.com/");
+    await page.getByRole("link", { name: "Sign In" }).waitFor({ state: "visible" });
+    await page.getByRole("link", { name: "Sign In" }).click();
+    await page.getByRole("heading", { name: "Login" }).waitFor({ state: "visible" }); 
+ });
+
+ When('the user enters valid credentials',async() => {
+  
+  await page.getByRole("textbox", { name: "email" }).fill("customer@practicesoftwaretesting.com");
+  await page.getByRole("textbox",{ name:"password"}).fill("welcome01");
+ } );
+
+ When('clicks the login button', async () => {
+    const loginButton = page.locator('[data-test="login-submit"]');
+    await loginButton.waitFor({ state: "visible" });
+    // await page.waitForLoadState("networkidle");
+    await loginButton.click();
+ } );
+
+ Then('the user should be redirected to the my account page', async () => {
+   
+    await page.getByRole("heading", { name: "My Account" }).waitFor({ state: "visible" }); 
+    const headingText = await page.getByRole("heading", { name: "My Account" }).innerText();
+    console.log("Heading text after login: ",headingText);
+    expect(headingText.trim()).toBe("My Account");
+     await expect(page).toHaveURL(/.*account/); 
+   
+ } );
+       
+
